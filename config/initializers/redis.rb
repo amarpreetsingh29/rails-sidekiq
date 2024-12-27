49d6fa7acccc5166881e54592808e9db96b57e77
Amarpreet Singh <amarpreet.talwar@gmail.com>
2024-12-26 19:03:34 -0800

    global vars for redis

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,20 +1,24 @@
+$redis_url = "#{ENV['REDIS_PROTOCOL']}://#{ENV['REDIS_HOST']}:#{ENV['REDIS_PORT']}/#{ENV['REDIS_DB']}"
+$sidekiq_redis_url = "#{ENV['REDIS_PROTOCOL']}://#{ENV['REDIS_SIDEKIQ_HOST']}:#{ENV['REDIS_SIDEKIQ_PORT']}/#{ENV['REDIS_SIDEKIQ_DB']}"
+$webhooks_redis_url = "#{ENV['REDIS_PROTOCOL']}://#{ENV['REDIS_SIDEKIQ_WEBHOOKS_HOST']}:#{ENV['REDIS_SIDEKIQ_WEBHOOKS_PORT']}/#{ENV['REDIS_SIDEKIQ_WEBHOOKS_DB']}"
+
 class RedisPool
-    class << self
-        attr_reader :default_pool
+  class << self
+    attr_reader :default_pool, :webhooks_pool
 
-        def init
-            sidekiq_redis_url = "#{ENV['REDIS_PROTOCOL']}://#{ENV['REDIS_SIDEKIQ_HOST']}:#{ENV['REDIS_SIDEKIQ_PORT']}/#{ENV['REDIS_SIDEKIQ_DB']}"
-            @default_pool = connection_pool(sidekiq_redis_url)
-        end
-
-        private
-
-        def connection_pool(redis_url)
-            ConnectionPool.new(size: 25) do
-                Redis.new(url: redis_url)
-            end
-        end
+    def init
+      @default_pool = connection_pool($sidekiq_redis_url)
+      @webhooks_pool = connection_pool($webhooks_redis_url)
     end
+
+    private
+
+    def connection_pool(redis_url)
+      ConnectionPool.new(size: 25) do
+          Redis.new(url: redis_url)
+      end
+    end
+  end
 end
 
 RedisPool.init
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,7 @@
-sidekiq_redis_url = "#{ENV['REDIS_PROTOCOL']}://#{ENV['REDIS_SIDEKIQ_HOST']}:#{ENV['REDIS_SIDEKIQ_PORT']}/#{ENV['REDIS_SIDEKIQ_DB']}"
-
 Sidekiq.configure_server do |config|
-  config.redis = { url:sidekiq_redis_url }
+  config.redis = { url: $sidekiq_redis_url }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: sidekiq_redis_url }
+  config.redis = { url: $sidekiq_redis_url }
 end
