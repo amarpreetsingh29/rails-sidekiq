554aefd9e1fc6def78576cdc78993fb7eb781af7
Amarpreet Singh <amarpreet.talwar@gmail.com>
2025-01-15 15:13:34 -0800

    use SERVICE_NAME env variable

--- config/initializers/opentelemetry.rb
+++ config/initializers/opentelemetry.rb
@@ -6,6 +6,6 @@ require 'opentelemetry-exporter-otlp'
 OpenTelemetry.logger.level = Logger::DEBUG
 
 OpenTelemetry::SDK.configure do |c|
-  c.service_name = 'dice-ruby'
+  c.service_name = ENV['SERVICE_NAME']
   c.use_all() # enables all instrumentation!
 end
