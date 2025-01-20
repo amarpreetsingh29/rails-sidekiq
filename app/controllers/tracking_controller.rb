1b06f770c37774ecfe60307fbc5da1f327bf5e98
Amarpreet Singh <amarpreet.talwar@gmail.com>
2025-01-20 13:13:46 -0800

    tracking number create

--- app/controllers/tracking_controller.rb
+++ app/controllers/tracking_controller.rb
@@ -19,4 +19,20 @@ class TrackingController < ApplicationController
 
     render json: tracking_number, status: 200, content_type: "application/json"
   end
+
+  def create
+    if params[:carrier].blank? || params[:tracking_number].blank?
+      return render json: {error: "Carrier and tracking number are required"}, status: :unprocessable_entity
+    end
+
+    carrier = Carrier.find_by(name: params[:carrier].upcase)
+    return render json: {error: "Not Found"}, status: :not_found unless carrier.present?
+
+    tracking_number = TrackingNumber.create!(
+      tag: params[:tracking_number],
+      carrier: carrier
+    )
+
+    render json: tracking_number, status: 201, content_type: "application/json"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,5 @@ Rails.application.routes.draw do
   get '/tracking/carriers', to: 'tracking#carriers'
   post '/tracking/generate_dummy_events', to: 'tracking#generate_dummy_events'
   get '/tracking/:id/events', to: 'tracking#events'
+  post '/tracking', to: 'tracking#create'
 end
